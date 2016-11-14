ea39fb7913e7af37cdc2a7d70fbd3e4aed0e0cb2
Brett g Porter <bgp@bgporter.net>
2016-11-14 08:27:06 -0500

    Put create tweet/fave calls inside exception handler

--- tmbotg.py
+++ tmbotg.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env/python
 
 # Copyright (c) 2013 Brett g Porter
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -40,7 +40,7 @@ from jsonSettings import JsonSettings as Settings
 import jsonSettings
 
 
-# if we're started without a config file, we create a default/empty 
+# if we're started without a config file, we create a default/empty
 # file that the user can fill in and then restart the app.
 kDefaultConfigDict = {
    "appKey"             : "!!! Your app's 'Consumer Key'",
@@ -77,16 +77,16 @@ class BotStreamer(TwythonStreamer):
       self.path = path
 
    def on_success(self, data):
-      # for now, all we're interested in handling are quoted tweets. 
+      # for now, all we're interested in handling are quoted tweets.
       if 'event' in data:
          if data['event'] == 'quoted_tweet':
             # get the id of the tweet that quotes us:
             tweetId = data['target_object']['id_str']
             # create a file that the other (periodic) cron-job bot process will handle.
-            # As very rudimentary IPC, we use the id of the tweet that quotes us as 
+            # As very rudimentary IPC, we use the id of the tweet that quotes us as
             # a filename. If we want to do more using the streaming API, we might want to write
             # more interesting data into the file to be parsed out later.
-            # Example -- we may want to extend the replies to questions so that we also 
+            # Example -- we may want to extend the replies to questions so that we also
             # reply to questions in quote tweets as well.
             with open(os.path.join(self.path, "{0}.fav".format(tweetId)), "wt") as f:
                f.write("{0}".format(tweetId))
@@ -96,10 +96,10 @@ class BotStreamer(TwythonStreamer):
       print "ERROR: {0}".format(status_code)
       self.disconnect()
 
-         
- 
+
+
 def ParseFilename(filePath):
-   ''' we name files like "Album-Title_Track-Title.lyric". This function breaks 
+   ''' we name files like "Album-Title_Track-Title.lyric". This function breaks
       a string in that format apart and returns a tuple ("Album-Title", "Track-Title")
    '''
    path, fileName = os.path.split(filePath)
@@ -110,7 +110,7 @@ def ParseFilename(filePath):
 def TrimTweetToFit(listOfStrings, maxLength):
    '''
       Given a list of strings (one string per line), trim that list (usually from
-      the back, but occasionally from the front to keep things interesting) until 
+      the back, but occasionally from the front to keep things interesting) until
       it's less than or equal to the maxLength parameter.
    '''
    length = 0
@@ -125,7 +125,7 @@ def TrimTweetToFit(listOfStrings, maxLength):
          # we can't trim any more because there's only one line left. Give up.
          return ""
       else:
-         # decoding this next line: create a list that weights the probability of 
+         # decoding this next line: create a list that weights the probability of
          # whether we trim lines from the end or the front (more frequently from the end)
          popIndex = choice([-1] * 8 + [0] * 2)
          listOfStrings.pop(popIndex)
@@ -147,11 +147,11 @@ class TmBot(object):
       if not argDict:
          argDict = { 'debug' : False, "force": False, 'stream': False, 'botPath' : "."}
       # update this object's internal dict with the dict of args that was passed
-      # in so we can access those values as attributes.   
+      # in so we can access those values as attributes.
       self.__dict__.update(argDict)
 
-      # we build a list of dicts containing status (and whatever other args 
-      # we may need to pass to the update_status function as we exit, most 
+      # we build a list of dicts containing status (and whatever other args
+      # we may need to pass to the update_status function as we exit, most
       # probably 'in_reply-to_status_id' when we're replying to someone.)
       self.tweets = []
 
@@ -167,7 +167,7 @@ class TmBot(object):
    def GetPath(self, path):
       '''
          Put all the relative path calculations in one place. If we're given a path
-         that has a leading slash, we treat it as absolute and do nothing. Otherwise, 
+         that has a leading slash, we treat it as absolute and do nothing. Otherwise,
          we treat it as a relative path based on the botPath setting in our config file.
       '''
       if not path.startswith(os.sep):
@@ -182,7 +182,7 @@ class TmBot(object):
          timestamp = integer seconds since the UNIX epoch
          event = string identifying the event
          data1..n = individual data fields, as appropriate for each event type.
-         To avoid maintenance issues w/r/t enormous log files, the log filename 
+         To avoid maintenance issues w/r/t enormous log files, the log filename
          that's stored in the settings file is passed through datetime.strftime()
          so we can expand any format codes found there against the current date/time
          and create e.g. a monthly log file.
@@ -201,20 +201,23 @@ class TmBot(object):
          f.write("\n")
 
    def SendTweets(self):
-      ''' send each of the status updates that are collected in self.tweets 
+      ''' send each of the status updates that are collected in self.tweets
       '''
       for msg in self.tweets:
          if self.debug:
             print msg['status'].encode("UTF-8")
          else:
-            self.twitter.update_status(**msg)
+            try:
+               self.twitter.update_status(**msg)
+            except TwythonError, e:
+               Log("EXCEPTION", [str(e), msg['status'].encode("UTF-8")])
 
 
    def CheckDaySpacing(self, album, title):
-      ''' There are a few tunes that seem to come up *way* too often. We'll maintain 
+      ''' There are a few tunes that seem to come up *way* too often. We'll maintain
          a new history file that just tracks the last time that any given (album, track) tuple
-         is used (and maybe this should just be title?). If we're okay to use this song, return 
-         true. 
+         is used (and maybe this should just be title?). If we're okay to use this song, return
+         true.
       '''
       key = "{0}_{1}".format(album, title)
       retval = True
@@ -241,12 +244,12 @@ class TmBot(object):
 
    def CreateUpdate(self):
       '''
-         Called everytime the bot is Run(). 
+         Called everytime the bot is Run().
          If a random number is less than the probability that we should generate
          a tweet (or if we're told to force one), we look into the lyrics database
          and (we hope) append a status update to the list of tweets.
 
-         1/11/14: Added a configurable 'minimumSpacing' variable to prevent us from 
+         1/11/14: Added a configurable 'minimumSpacing' variable to prevent us from
          posting an update too frequently. Starting at an hour ()
 
       '''
@@ -266,7 +269,7 @@ class TmBot(object):
          doUpdate = True
 
       elif random() < self.settings.tweetProbability:
-         # Make sure that we're not tweeting too frequently. Default is to enforce 
+         # Make sure that we're not tweeting too frequently. Default is to enforce
          # a 1-hour gap between tweets (configurable using the 'minimumSpacing' key
          # in the config file, providing a number of minutes we must remain silent.)
          requiredSpace = self.settings.minimumSpacing
@@ -297,7 +300,7 @@ class TmBot(object):
       '''
          Get all the tweets that mention us since the last time we ran and process each
          one.
-         Any time we're mentioned in someone's tweet, we favorite it. If they ask 
+         Any time we're mentioned in someone's tweet, we favorite it. If they ask
          us a question, we reply to them.
       '''
       mentions = self.twitter.get_mentions_timeline(since_id=self.settings.lastMentionId)
@@ -313,12 +316,16 @@ class TmBot(object):
             if self.debug:
                print "Faving tweet {0} by {1}:\n {2}".format(theId, who, text.encode("utf-8"))
             else:
-               self.twitter.create_favorite(id=theId)
-            
+               try:
+                  self.twitter.create_favorite(id=theId)
+               except TwythonError, e:
+                  Log("EXCEPTION", [str(e), msg['status'].encode("UTF-8")])
+
+
             eventType = 'Mention'
-            # if they asked us a question, reply to them.   
+            # if they asked us a question, reply to them.
             if "?" in text:
-               # create a reply to them. 
+               # create a reply to them.
                maxReplyLen = 120 - len(who)
                album, track, msg = self.GetLyric(maxReplyLen)
                # get just the first line
@@ -335,8 +342,8 @@ class TmBot(object):
 
    def HandleQuotes(self):
       ''' The streaming version of the bot may have detected some quoted tweets
-         that we want to respond to. Look for files with the .fav extension, and 
-         if we find any, handle them. 
+         that we want to respond to. Look for files with the .fav extension, and
+         if we find any, handle them.
       '''
       faves = glob(self.GetPath("*.fav"))
       for fileName in faves:
@@ -357,8 +364,8 @@ class TmBot(object):
          if self.debug:
             print "About to stream from user account."
          try:
-            # The call to user() will sit forever waiting for events on 
-            # our user account to stream down. Those events will be handled 
+            # The call to user() will sit forever waiting for events on
+            # our user account to stream down. Those events will be handled
             # for us by the BotStreamer object that we created ab
             self.twitter.user()
          except KeyboardInterrupt:
@@ -377,17 +384,17 @@ class TmBot(object):
 
 
    def GetLyric(self, maxLen, count=10):
-      ''' open a random lyric file, then grab a random stanza of lyrics from it, 
+      ''' open a random lyric file, then grab a random stanza of lyrics from it,
          then (if needed) trim it down into lines <= maxLen
          returns a tuple (album, track, stanza) (we may want to log the album/tracks
             that are being used...)
 
          If we don't immediately find a random chunk of text that meets the maxLen
-         criteria, we call ourself recursively, decrementing the count parameter until 
-         it hits zero, at which point we give up and raise an exception. Obviously, we 
-         could look for a longer time, or come up with a search algorithm to find text 
-         meets the current length criteria, or, or, or..., but I actually like the idea 
-         that it's possible to occasionally just throw up our hands and give up. We'll 
+         criteria, we call ourself recursively, decrementing the count parameter until
+         it hits zero, at which point we give up and raise an exception. Obviously, we
+         could look for a longer time, or come up with a search algorithm to find text
+         meets the current length criteria, or, or, or..., but I actually like the idea
+         that it's possible to occasionally just throw up our hands and give up. We'll
          try again in a bit.
       '''
       if 0 == count:
@@ -420,12 +427,12 @@ class TmBot(object):
 if __name__ == "__main__":
    import argparse
    parser = argparse.ArgumentParser()
-   parser.add_argument("--debug", action='store_true', 
+   parser.add_argument("--debug", action='store_true',
       help="print to stdout instead of tweeting")
-   parser.add_argument("--force", action='store_true', 
+   parser.add_argument("--force", action='store_true',
       help="force operation now instead of waiting for randomness")
 
-   parser.add_argument("--stream", action="store_true", 
+   parser.add_argument("--stream", action="store_true",
       help="run in streaming mode")
    args = parser.parse_args()
    # convert the object returned from parse_args() to a plain old dict
@@ -444,4 +451,3 @@ if __name__ == "__main__":
       # !!! TODO: Write this into a log file (also)
       bot.Log("ERROR", [str(e)])
       print str(e)
-
